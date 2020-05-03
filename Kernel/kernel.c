2f0aaec79d8ea5f4352c3d981c0b1bcbbbc882da
abdullahsultan <abdullahsultan448@gmail.com>
2020-05-04 03:40:45 +0500

    DriverKernelBindError

--- Kernel/kernel.c
+++ Kernel/kernel.c
@@ -1,6 +1,11 @@
-
+#include "../Drivers/screen.h"
 void main ()
 {
-      char * video_memory = (char *) 0xb8000 ;
-      * video_memory = 'X';
+
+    clear_screen();
+    kprint_at("X", 1, 6);
+    kprint_at("This text spans multiple lines", 75, 10);
+    kprint_at("There is a line\nbreak", 0, 20);
+    kprint("There is a line\nbreak");
+kprint_at("What happens when we run out of space?", 45, 24);
 }
